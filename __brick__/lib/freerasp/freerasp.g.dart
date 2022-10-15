1c89608fc2ebb45166c9de0f24033d5e7add7919
Jaroslav Novotny <novotnyj912@gmail.com>
2022-10-15 19:56:00 +0200

    feat(config-generation): Added generation templates

---
+++ __brick__/lib/freerasp/freerasp.g.dart
@@ -0,0 +1,19 @@
+import 'package:freerasp/talsec_app.dart';
+part 'freerasp_callback.g.dart';
+
+TalsecConfig _config = TalsecConfig(
+  watcherMail: '{{watcher_mail}}',{{#android}}
+  androidConfig: AndroidConfig(
+    expectedPackageName: '{{package_name}}',
+    expectedSigningCertificateHash: '{{signing_hash}}',
+  ),{{/android}}{{#ios}}
+  iosConfig: IOSconfig(
+    appBundleId: '{{bundle_id}}',
+    appTeamId: '{{team_id}}',
+  ),{{/ios}}
+);
+
+TalsecApp talsec = TalsecApp(
+  config: _config,
+  callback: callback,
+);
---
+++ __brick__/lib/freerasp/freerasp_callback.g.dart
@@ -0,0 +1,23 @@
+part of 'freerasp.g.dart';
+
+final callback = TalsecCallback({{#android}}
+  androidCallback: AndroidCallback(
+    onRootDetected: () => print('root'),
+    onEmulatorDetected: () => print('emulator'),
+    onHookDetected: () => print('hook'),
+    onTamperDetected: () => print('tamper'),
+    onDeviceBindingDetected: () => print('device binding'),
+    onUntrustedInstallationDetected: () => print('untrusted install'),
+  ), {{/android}} {{#ios}}
+  iosCallback: IOSCallback(
+      onSignatureDetected: () => print('signature'),
+      onRuntimeManipulationDetected: () => print('runtime manipulation'),
+      onJailbreakDetected: () => print('jailbreak'),
+      onPasscodeDetected: () => print('passcode'),
+      onSimulatorDetected: () => print('simulator'),
+      onMissingSecureEnclaveDetected: () => print('secure enclave'),
+      onDeviceChangeDetected: () => print('device change'),
+      onDeviceIdDetected: () => print('device ID'),
+      onUnofficialStoreDetected: () => print('unofficial store')), {{/ios}}
+  onDebuggerDetected: () => print('debugger'),
+);
