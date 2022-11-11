0c8a641603891c82174b0260e192c230b77ac656
Jaroslav Novotný <novotnyj912@gmail.com>
2022-11-11 23:49:45 +0100

    refactoring(error): Parameters are named

--- hooks/errors/freerasp_brick_exception.dart
+++ hooks/errors/freerasp_brick_exception.dart
@@ -1,19 +1,18 @@
 class FreeRaspBrickException implements Exception {
-
   const FreeRaspBrickException({
     this.code = 'brick-failure',
     this.message,
     this.stackTrace,
   });
 
-  factory FreeRaspBrickException.wtf(StackTrace? stackTrace) {
+  factory FreeRaspBrickException.wtf({StackTrace? stackTrace}) {
     return FreeRaspBrickException(
       message: 'Unexpected error occurred.',
       stackTrace: stackTrace,
     );
   }
 
-  factory FreeRaspBrickException.gradleUpdateFailure(StackTrace? stackTrace) {
+  factory FreeRaspBrickException.gradleUpdateFailure({StackTrace? stackTrace}) {
     return FreeRaspBrickException(
       code: 'gradle-failure',
       message: 'Issue occurred during update of gradle file.',
